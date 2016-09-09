c4e13ce743199d44fe2b65a95458dae6c19f8e13
AhJin Noh <ahjinnoh@gmail.com>
2016-09-09 17:09:11 -0400

    AJ filled in the constructor (P3)

--- M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -65,7 +65,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p2.toString("gburdell2");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("AhJin Noh");
+		  Person3 p3 = new Person3("Ah Jin Noh");
 		  toPrint += p3.toString("anoh30");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
